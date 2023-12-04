6bf00eb481e055e893c2f21e988c0808b868ab8e
AlanPassucci <alan.passucci@mercadolibre.com>
2023-12-04 17:26:02 -0300

    Hago el Ej03

--- 06-Errores/Ej03/main.go
+++ 06-Errores/Ej03/main.go
@@ -6,6 +6,36 @@ se implemente “errors.New()”.
 
 package main
 
-func main() {
+import (
+	"errors"
+	"fmt"
+)
 
+var (
+	ErrNotTaxableSalary = "salary is less than 10000"
+)
+
+func ShouldPayTaxes(salary int) (err error) {
+	if salary <= 10000 {
+		err = errors.New(ErrNotTaxableSalary)
+		return
+	}
+	return
+}
+
+func main() {
+	const salary = 8000
+
+	if err := ShouldPayTaxes(salary); err != nil {
+		switch err.Error() {
+		case ErrNotTaxableSalary:
+			fmt.Println(err.Error())
+			return
+		default:
+			fmt.Println("an unexpected error ocurred")
+			return
+		}
+	}
+
+	fmt.Println("Must pay tax")
 }
